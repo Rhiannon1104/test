27a0ce477b13d3f83b14af2da583936e6cf00257
safeNutShell <46984866+safeNutShell@users.noreply.github.com>
2020-02-28 21:11:15 +0800

    试一试能传哪些文件以及如何branch
    3个文件

---
+++ Node.java
@@ -0,0 +1,14 @@
+package K_means;
+
+public class Node {
+    double x1;//第一维数据
+    double x2;//第二维数据
+    int nodeID;//节点编号
+
+    Node(double x1, double x2, int ID){
+        super();
+        this.x1 = x1;
+        this.x2 = x2;
+        this.nodeID = ID;
+    }
+}
